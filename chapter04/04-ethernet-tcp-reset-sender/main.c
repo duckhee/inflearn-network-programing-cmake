4848d27df2e7e71b8022a021bcb63a7055ff7c7a
Won <fain9301@yahoo.com>
2024-11-12 18:23:37 +0900

    feat: set tcp reset setting

--- chapter04/04-ethernet-tcp-reset-sender/main.c
+++ chapter04/04-ethernet-tcp-reset-sender/main.c
@@ -81,64 +81,65 @@ unsigned short CalculateChecksumIP(IpHeader_t *pIpHeader);
 unsigned short CalculateChecksumTCP(IpHeader_t *pIpHeader, TcpHeader_t *pTcpHeader);
 
 int main(int argc, char **argv) {
-    pcap_if_t *pAllDevice;
+    pcap_if_t *allDevice;
     pcap_if_t *pDevice;
     pcap_t *pHandler;
-    int deviceCounter = 0;
-    int selectDevice = 0;
-    char errorLog[PCAP_ERRBUF_SIZE];
+    unsigned int deviceCounter = 0;
+    unsigned int selectDevice;
+    char pcapError[PCAP_ERRBUF_SIZE] = {0,};
 
-    if (!LoadDllNpcap()) {
-        fprintf(stderr, "Couldn't load npcap\r\n");
+    //packet을 보내기 위해서는 pcap에 대한 초기화를 해줘야 한다.
+    if (pcap_init(PCAP_CHAR_ENC_LOCAL, pcapError)) {
+        fprintf(stderr, "Failed to initialize pcap lib: %s\n", pcapError);
+        fflush(stderr);
+        return 2;
+    }
+
+    if (pcap_findalldevs(&allDevice, pcapError) == -1) {
+        fprintf(stderr, "can not found NIC....\r\n");
+        fflush(stderr);
         return -1;
     }
 
-    /** pcap initialized check */
-    if (pcap_init(PCAP_CHAR_ENC_LOCAL, errorLog) != 0) {
-        fprintf(stderr, "Failed pcap initialized : %s\r\n", errorLog);
-        return -1;
-    }
-
-    if (pcap_findalldevs(&pAllDevice, errorLog) == -1) {
-        fprintf(stderr, "Error in pcap_findalldevice: %s\r\n", errorLog);
-        return -1;
-    }
-
-    for (pDevice = pAllDevice; pDevice != NULL; pDevice = pDevice->next) {
+    for (pDevice = allDevice; pDevice != NULL; pDevice = pDevice->next) {
         printf("%d. %s ", ++deviceCounter, pDevice->name);
         if (pDevice->description) {
             printf("(%s)\r\n", pDevice->description);
         } else {
-            printf(" (No description available)\n");
+            printf("(No Description available)\r\n");
         }
     }
 
     if (deviceCounter == 0) {
-        printf("\nNo interfaces found! Make sure Npcap is installed.\n");
+        printf("\r\nNo Interface found! Make sure Npcap is installed\r\n");
         return -1;
     }
 
-    printf("Enter the interface number (1-%d):", deviceCounter);
+    printf("Enter the interface number(1 - %d) : ", deviceCounter);
     scanf_s("%d%*c", &selectDevice);
 
     if (selectDevice < 1 || selectDevice > deviceCounter) {
-        printf("\nInterface number out of range.\n");
-        /* Free the device list */
-        pcap_freealldevs(pAllDevice);
+        printf("\r\nInterface number out of range\r\n");
+        pcap_freealldevs(allDevice);
         return -1;
     }
 
-    for (pDevice = pAllDevice, deviceCounter = 0;
+    for (pDevice = allDevice, deviceCounter = 0;
          deviceCounter < selectDevice - 1; pDevice = pDevice->next, deviceCounter++);
 
-    printf("[Ethernet message sender]\n");
-
-    if ((pHandler = pcap_open_live(pDevice->name, 0, 0, 1000, errorLog)) == NULL) {
-        fprintf(stderr, "\nUnable to open the adapter. %s is not supported by Npcap\n", pDevice->name);
+    if ((pHandler = pcap_open_live(
+            pDevice->name,
+            0,
+            0,
+            1000,
+            pcapError
+    )) == NULL) {
+        fprintf(stderr, "failed open device : %s, [ERROR] : %s\r\n", pDevice->name, pcapError);
+        fflush(stderr);
         return -1;
     }
 
-    pcap_freealldevs(pAllDevice);
+    pcap_freealldevs(allDevice);
 
     unsigned char frameData[1514] = {0,};
     int msgSize = 0;
@@ -147,67 +148,69 @@ int main(int argc, char **argv) {
 
     pEthernetHeader->type = htons(0x0800);
     /** TODO Set Destination Mac Address */
-    pEthernetHeader->dstMac[0] = 0x00;
-    pEthernetHeader->dstMac[1] = 0x00;
-    pEthernetHeader->dstMac[2] = 0x00;
-    pEthernetHeader->dstMac[3] = 0x00;
-    pEthernetHeader->dstMac[4] = 0x00;
-    pEthernetHeader->dstMac[5] = 0x00;
+    pEthernetHeader->dstMac[0] = 0x0C;
+    pEthernetHeader->dstMac[1] = 0x9A;
+    pEthernetHeader->dstMac[2] = 0x3C;
+    pEthernetHeader->dstMac[3] = 0xE2;
+    pEthernetHeader->dstMac[4] = 0xB6;
+    pEthernetHeader->dstMac[5] = 0x02;
 
     /** TODO Set Source Mac Address */
-    pEthernetHeader->srcMac[0] = 0x00;
-    pEthernetHeader->srcMac[1] = 0x00;
-    pEthernetHeader->srcMac[2] = 0x00;
-    pEthernetHeader->srcMac[3] = 0x00;
-    pEthernetHeader->srcMac[4] = 0x00;
-    pEthernetHeader->srcMac[5] = 0x00;
+    pEthernetHeader->srcMac[0] = 0xB0;
+    pEthernetHeader->srcMac[1] = 0x47;
+    pEthernetHeader->srcMac[2] = 0xE9;
+    pEthernetHeader->srcMac[3] = 0x75;
+    pEthernetHeader->srcMac[4] = 0x2E;
+    pEthernetHeader->srcMac[5] = 0x20;
 
     IpHeader_t *pIpHeader = (IpHeader_t *) (frameData + sizeof(EthernetHeader_t));
 
     /** TODO Set Destination IP */
-    pIpHeader->dstIp[0] = 0;
-    pIpHeader->dstIp[1] = 0;
-    pIpHeader->dstIp[2] = 0;
-    pIpHeader->dstIp[3] = 0;
+    pIpHeader->dstIp[0] = 192;
+    pIpHeader->dstIp[1] = 168;
+    pIpHeader->dstIp[2] = 45;
+    pIpHeader->dstIp[3] = 51;
 
     /** TODO Set Source IP */
-    pIpHeader->srcIp[0] = 0;
-    pIpHeader->srcIp[1] = 0;
-    pIpHeader->srcIp[2] = 0;
-    pIpHeader->srcIp[3] = 0;
+    pIpHeader->srcIp[0] = 192;
+    pIpHeader->srcIp[1] = 168;
+    pIpHeader->srcIp[2] = 45;
+    pIpHeader->srcIp[3] = 26;
 
     /** ip version and header size setting */
     int ipHeaderSize = sizeof(IpHeader_t);
     printf("ip Header Size : %d\r\n", ipHeaderSize / 4);
     pIpHeader->verIhl = 0x45;
     pIpHeader->tos = 0x00;
-    pIpHeader->length = 20;
+    pIpHeader->length = htons(40);
     pIpHeader->id = 0x3412;
     pIpHeader->fragOffset = htons(0x4000); // DF Set -> 단편화 사용 설정
     pIpHeader->ttl = 0xFF;
     pIpHeader->protocol = 0x06; // TCP protocl setting
     pIpHeader->checksum = 0x0000;
 
-    TcpHeader_t *pTcpHeader = (TcpHeader_t *)(frameData + sizeof(EthernetHeader_t) + ipHeaderSize);
+    TcpHeader_t *pTcpHeader = (TcpHeader_t *) (frameData + sizeof(EthernetHeader_t) + ipHeaderSize);
 
     /** tcp header setting */
     /** TODO TCP Chatting Client Port Matching */
-    pTcpHeader->srcPort = htons(55985);
+    pTcpHeader->srcPort = htons(64972);
     pTcpHeader->dstPort = htons(25000);
     /** TODO Checking client last sequence number Using wire shark */
-    pTcpHeader->seq = htonl(0x1def89c2);
-
+    pTcpHeader->seq = htonl(0x2f81db84);
+    pTcpHeader->ack = 0;
     pTcpHeader->data = 0x50;
     /** RESET Flag Setting */
     pTcpHeader->flags = 0x04;
     pTcpHeader->windowSize = 0x00;
+    pTcpHeader->urgent = 0;
 
     pTcpHeader->checksum = 0x0000;
 
     pIpHeader->checksum = CalculateChecksumIP(pIpHeader);
     pTcpHeader->checksum = CalculateChecksumTCP(pIpHeader, pTcpHeader);
 
-    if(pcap_sendpacket(pHandler, frameData, sizeof(EthernetHeader_t ) + sizeof(IpHeader_t ) +sizeof(TcpHeader_t)) != 0){
+    if (pcap_sendpacket(pHandler, frameData, sizeof(EthernetHeader_t) + sizeof(IpHeader_t) + sizeof(TcpHeader_t)) !=
+        0) {
         fprintf(stderr, "\r\nError sending the packet: %s\r\n", pcap_geterr(pHandler));
     }
 
@@ -217,22 +220,19 @@ int main(int argc, char **argv) {
 }
 
 
-
-unsigned short CalculateChecksumIP(IpHeader_t *pIpHeader){
+unsigned short CalculateChecksumIP(IpHeader_t *pIpHeader) {
     unsigned char ihl = (pIpHeader->verIhl & 0x0F) << 2; //*4와 동일
-    unsigned short wData[30] = { 0 };
+    unsigned short wData[30] = {0};
     unsigned int dwSum = 0;
 
-    memcpy(wData, (BYTE*)pIpHeader, ihl);
+    memcpy(wData, (BYTE *) pIpHeader, ihl);
     //((IpHeader*)wData)->checksum = 0x0000;
 
-    for (int i = 0; i < ihl / 2; i++)
-    {
+    for (int i = 0; i < ihl / 2; i++) {
         if (i != 5)
             dwSum += wData[i];
 
-        if (dwSum & 0xFFFF0000)
-        {
+        if (dwSum & 0xFFFF0000) {
             dwSum &= 0x0000FFFF;
             dwSum++;
         }
@@ -241,18 +241,18 @@ unsigned short CalculateChecksumIP(IpHeader_t *pIpHeader){
     return ~(dwSum & 0x0000FFFF);
 }
 
-unsigned short CalculateChecksumTCP(IpHeader_t *pIpHeader, TcpHeader_t *pTcpHeader){
-    PseudoHeader_t	pseudoHeader = { 0 };
-    unsigned short* pwPseudoHeader = (unsigned short*)&pseudoHeader;
-    unsigned short* pwDatagram = (unsigned short*)pTcpHeader;
-    int				nPseudoHeaderSize = 6; //WORD 6개 배열
-    int				nSegmentSize = 0; //헤더 포함
+unsigned short CalculateChecksumTCP(IpHeader_t *pIpHeader, TcpHeader_t *pTcpHeader) {
+    PseudoHeader_t pseudoHeader = {0};
+    unsigned short *pwPseudoHeader = (unsigned short *) &pseudoHeader;
+    unsigned short *pwDatagram = (unsigned short *) pTcpHeader;
+    int nPseudoHeaderSize = 6; //WORD 6개 배열
+    int nSegmentSize = 0; //헤더 포함
 
-    UINT32			dwSum = 0;
-    int				nLengthOfArray = 0;
+    UINT32 dwSum = 0;
+    int nLengthOfArray = 0;
 
-    pseudoHeader.srcIp = *(unsigned int*)pIpHeader->srcIp;
-    pseudoHeader.dstIp = *(unsigned int*)pIpHeader->dstIp;
+    pseudoHeader.srcIp = *(unsigned int *) pIpHeader->srcIp;
+    pseudoHeader.dstIp = *(unsigned int *) pIpHeader->dstIp;
     pseudoHeader.zero = 0;
     pseudoHeader.protocol = 6;
     pseudoHeader.length = htons(ntohs(pIpHeader->length) - 20);
@@ -265,26 +265,22 @@ unsigned short CalculateChecksumTCP(IpHeader_t *pIpHeader, TcpHeader_t *pTcpHead
     else
         nLengthOfArray = nSegmentSize / 2;
 
-    for (int i = 0; i < nPseudoHeaderSize; i++)
-    {
+    for (int i = 0; i < nPseudoHeaderSize; i++) {
         dwSum += pwPseudoHeader[i];
-        if (dwSum & 0xFFFF0000)
-        {
+        if (dwSum & 0xFFFF0000) {
             dwSum &= 0x0000FFFF;
             dwSum++;
         }
     }
 
-    for (int i = 0; i < nLengthOfArray; i++)
-    {
+    for (int i = 0; i < nLengthOfArray; i++) {
         if (i != 8)
             dwSum += pwDatagram[i];
-        if (dwSum & 0xFFFF0000)
-        {
+        if (dwSum & 0xFFFF0000) {
             dwSum &= 0x0000FFFF;
             dwSum++;
         }
     }
 
-    return (USHORT)~(dwSum & 0x0000FFFF);
+    return (USHORT) ~(dwSum & 0x0000FFFF);
 }
--- chapter04/04-tcp-chatting-client/main.cpp
+++ chapter04/04-tcp-chatting-client/main.cpp
@@ -1,32 +1,29 @@
 #include <iostream>
-#include <winsock2.h>
+#include <WinSock2.h>
 #include <ws2tcpip.h>
+#include <Windows.h>
 
 DWORD WINAPI ThreadReceiver(LPVOID pParam);
 
-void ErrorHandler(const char* msg);
+void ErrorHandler(const char *msg);
 
-int main(int argc, char* argv[])
-{
-    WSAData wsaData;
-    if (WSAStartup(MAKEWORD(2, 2), &wsaData) == 0)
-    {
+int main(int argc, char *argv[]) {
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
         ErrorHandler("Failed Window socket Initialize...");
     }
 
     SOCKET hClient = ::socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if (hClient == INVALID_SOCKET)
-    {
+    if (hClient == INVALID_SOCKET) {
         ErrorHandler("Failed Socket Creation...");
     }
 
     SOCKADDR_IN serverAddr = {0,};
     serverAddr.sin_family = PF_INET;
-    inet_pton(PF_INET, "<IP>", &serverAddr.sin_addr);
+    inet_pton(PF_INET, "192.168.45.51", &serverAddr.sin_addr);
     serverAddr.sin_port = htons(25000);
 
-    if (::connect(hClient, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
-    {
+    if (::connect(hClient, (SOCKADDR *) &serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
         ErrorHandler("Failed Socket Connection...");
     }
 
@@ -35,7 +32,7 @@ int main(int argc, char* argv[])
             NULL,
             0,
             ThreadReceiver,
-            (LPVOID)hClient,
+            (LPVOID) hClient,
             0,
             &dwThreadID
     );
@@ -45,16 +42,14 @@ int main(int argc, char* argv[])
     char szBuffer[128];
     puts("채팅을 시작합니다. 메시지를 입력하세요.");
 
-    while (true)
-    {
+    while (true) {
         memset(szBuffer, 0, sizeof(szBuffer));
         gets_s(szBuffer);
-        if (strcmp(szBuffer, "EXIT") == 0)
-        {
+        if (strcmp(szBuffer, "EXIT") == 0) {
             break;
         }
 
-        ::send(hClient, szBuffer, (int)strlen(szBuffer) + 1, 0);
+        ::send(hClient, szBuffer, (int) strlen(szBuffer) + 1, 0);
     }
 
     ::closesocket(hClient);
@@ -65,12 +60,10 @@ int main(int argc, char* argv[])
 }
 
 
-DWORD WINAPI ThreadReceiver(LPVOID pParam)
-{
-    SOCKET hClient = (SOCKET)pParam;
+DWORD WINAPI ThreadReceiver(LPVOID pParam) {
+    SOCKET hClient = (SOCKET) pParam;
     char szBuffer[128] = {0,};
-    while (::recv(hClient, szBuffer, sizeof(szBuffer), 0) > 0)
-    {
+    while (::recv(hClient, szBuffer, sizeof(szBuffer), 0) > 0) {
         printf(" -> %s\r\n", szBuffer);
         memset(szBuffer, '\0', sizeof(szBuffer));
     }
@@ -78,8 +71,7 @@ DWORD WINAPI ThreadReceiver(LPVOID pParam)
     return 0;
 }
 
-void ErrorHandler(const char* msg)
-{
+void ErrorHandler(const char *msg) {
     fprintf(stderr, "ERROR : %s\r\n", msg);
     fflush(stderr);
     exit(1);
